1245050a62abe55e84a86292eb3f5293d96d3360
andsr157 <andikasatrio159@gmail.com>
2023-12-23 23:50:08 +0700

    first commit

---
+++ components/TheExperience.vue
@@ -0,0 +1,111 @@
+<script setup lang="ts">
+import { ref, onMounted, computed } from 'vue'
+import { useWindowSize } from '@vueuse/core'
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
+import {
+  Scene,
+  PerspectiveCamera,
+  WebGLRenderer,
+  Color,
+  Fog,
+  Quaternion,
+  Euler,
+  AmbientLight,
+  DirectionalLight,
+} from 'three'
+
+const experience = ref<HTMLCanvasElement | null>(null)
+const { width, height } = useWindowSize()
+const aspectRatio = computed(() => width.value / height.value)
+
+const bgColor = new Color('#161618')
+const scene = new Scene()
+
+scene.fog = new Fog(bgColor, 0.1, 75)
+scene.background = bgColor
+
+const camera = new PerspectiveCamera(75, aspectRatio.value, 0.1, 1000)
+camera.position.set(0, 0, 10)
+scene.add(camera)
+
+const gltfLoader = new GLTFLoader()
+let object: any
+
+onMounted(() => {
+  gltfLoader.load('/untitled.gltf', gltf => {
+    gltf.scene.scale.set(0.3, 0.3, 0.3)
+    gltf.scene.position.set(0, 0, 0)
+    scene.add(gltf.scene)
+    object = gltf.scene
+    setRenderer()
+    addLights()
+    loop()
+  })
+})
+
+let renderer: WebGLRenderer
+
+function updateCamera() {
+  camera.aspect = aspectRatio.value
+  camera.updateProjectionMatrix()
+}
+
+function updateRenderer() {
+  if (renderer) {
+    renderer.setSize(width.value, height.value)
+    renderer.render(scene, camera)
+  }
+}
+
+function setRenderer() {
+  if (experience.value) {
+    renderer = new WebGLRenderer({ canvas: experience.value, alpha: true })
+
+    // Add event listeners for mouseenter and mouseleave
+    experience.value.addEventListener('mouseenter', handleMouseEnter)
+    experience.value.addEventListener('mouseleave', handleMouseLeave)
+  }
+}
+
+function addLights() {
+  const ambientLight = new AmbientLight(0x404040)
+  scene.add(ambientLight)
+
+  const directionalLight = new DirectionalLight(0xffffff, 0.8)
+  directionalLight.position.set(1, 1, 1).normalize()
+  scene.add(directionalLight)
+}
+
+function loop() {
+  // Animation loop
+  if (object) {
+    // Rotate object in the local coordinate system only when not hovered
+    if (!object.userData.hovered) {
+      const deltaRotationQuaternion = new Quaternion().setFromEuler(new Euler(0, 0.005, 0))
+      object.quaternion.multiplyQuaternions(deltaRotationQuaternion, object.quaternion)
+    }
+  }
+
+  updateCamera()
+  updateRenderer()
+  requestAnimationFrame(loop)
+}
+
+function handleMouseEnter() {
+  if (object) {
+    object.userData.hovered = false
+  }
+}
+
+function handleMouseLeave() {
+  if (object) {
+    object.userData.hovered = true
+  }
+}
+</script>
+
+<template>
+  <div class="w-full h-full">
+    <canvas ref="experience" class="w-full h-full" />
+  </div>
+</template>
---
+++ components/sections/Content.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+const reverse = ref(false)
+</script>
+
+<template>
+  <div class="px-7">
+    <div class="flex gap-4 font-[Satoshi-Variable] font-medium text-sm">
+      <button
+        @mouseenter="reverse = true"
+        @mouseleave="reverse = false"
+        :class="reverse ? 'bg-transparent text-white border border-white' : 'bg-white text-black'"
+        class="rounded-md py-2 px-[10px] transition-colors duration-300"
+      >
+        Link Pendaftaran
+      </button>
+      <button
+        @mouseenter="reverse = true"
+        @mouseleave="reverse = false"
+        :class="reverse ? 'bg-white  text-black' : 'bg-transparent border border-white  text-white'"
+        class="rounded-md py-2 px-[10px] transition-colors duration-300"
+      >
+        Link Detail Divisi
+      </button>
+    </div>
+
+    <div>
+      <h1 class="leading-snug text-white font-[Satoshi-Variable] font-medium mt-6 text-[40px]">
+        Ignite possibilities in innovation and tech! Empower your journey with the Information Systems student
+        association. Your contributions fuel our evolution. Inspire success in the world of information systems!
+      </h1>
+    </div>
+  </div>
+</template>
